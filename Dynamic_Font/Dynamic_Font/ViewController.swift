a1393d235a48cb1ca7c496c2be56ec723bcc700f
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-29 13:33:06 +0900

    start Dynamic Font design by autolayout
    2021-10-29

---
+++ Dynamic_Font/Dynamic_Font/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynamic_Font
+//
+//  Created by 재영신 on 2021/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
