1cbc284d07a457a5b9d1c488ddd0d2ff1b3ad543
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-29 11:30:36 +0900

    modify Calc button autolayout to multiple stack view and add  button cornerRadius setting code
    2021-10-29

--- Calc_autolayout/Calc_autolayout/ViewController.swift
+++ Calc_autolayout/Calc_autolayout/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var parentStackView: UIStackView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultLabel.layer.cornerRadius = 50
+        resultLabel.layer.masksToBounds = true
+        //resultLabel.clipsToBounds = true
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        for view in parentStackView.arrangedSubviews{
+            if let stackView = view as? UIStackView
+            {
+                for btn in stackView.arrangedSubviews
+                {
+                    if let button = btn as? UIButton
+                    {
+                        button.layer.cornerRadius = button.bounds.height / 2
+                    }
+                }
+            }
+        }
+    }
 }
 
