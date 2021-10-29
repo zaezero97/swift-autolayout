bcc7b4296e87df9a295f1897ea581601de52c117
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-10-29 14:45:27 +0900

    start custom_keyboard design by autolayout
    2021 - 10 - 29

---
+++ Custom_keyboard/Custom_keyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom_keyboard
+//
+//  Created by 재영신 on 2021/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
