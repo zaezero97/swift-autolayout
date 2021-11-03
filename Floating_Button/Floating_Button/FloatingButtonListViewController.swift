a5436ccedbd4b5b7ee1d38e0a2e32dd08727c520
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-03 10:41:01 +0900

    add FloatingViewController, views that Floating button need.  for FloatingButton
    2021 - 11 -3

---
+++ Floating_Button/Floating_Button/FloatingButtonListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FloatingButtonListViewController.swift
+//  Floating_Button
+//
+//  Created by 재영신 on 2021/11/03.
+//
+
+import UIKit
+
+class FloatingButtonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- Floating_Button/Floating_Button/ViewController.swift
+++ Floating_Button/Floating_Button/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showPopup"{
+            let floatingVC = segue.destination as! FloatingButtonListViewController
+            floatingVC.modalPresentationStyle = .overCurrentContext
+            
+        }
+    }
 }
 
