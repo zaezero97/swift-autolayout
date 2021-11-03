96245bcccd04154f856d45c5312ffa710d188f23
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-03 13:12:21 +0900

    add changeMultiplier func to extention NSLayoutConstraint
    2021 - 11 -3

--- Chart_Multipler/Chart_Multipler/ViewController.swift
+++ Chart_Multipler/Chart_Multipler/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var graph1Height: NSLayoutConstraint!
+    @IBOutlet weak var graph2Height: NSLayoutConstraint!
+    @IBOutlet weak var graph3Height: NSLayoutConstraint!
+    @IBOutlet weak var graph4Height: NSLayoutConstraint!
+    @IBOutlet weak var graph5Height: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func style1(_ sender: Any) {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5, options: .curveEaseOut) {
+            self.graph1Height = self.graph1Height.changeMultiplier(value: 0.5)
+            self.graph2Height = self.graph2Height.changeMultiplier(value: 0.6)
+            self.graph3Height = self.graph3Height.changeMultiplier(value: 0.7)
+            self.graph4Height = self.graph4Height.changeMultiplier(value: 0.8)
+            self.graph5Height = self.graph5Height.changeMultiplier(value: 0.9)
+            self.view.layoutIfNeeded()
+        } completion: { (completion) in
+            
+        }
 
+        
+    }
+    @IBAction func style2(_ sender: Any) {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5, options: .curveEaseOut) {
+            self.graph1Height = self.graph1Height.changeMultiplier(value: 0.9)
+            self.graph2Height = self.graph2Height.changeMultiplier(value: 0.8)
+            self.graph3Height = self.graph3Height.changeMultiplier(value: 0.7)
+            self.graph4Height = self.graph4Height.changeMultiplier(value: 0.6)
+            self.graph5Height = self.graph5Height.changeMultiplier(value: 0.5)
+            self.view.layoutIfNeeded()
+        } completion: { (completion) in
+            
+        }
+    }
 }
 
+extension NSLayoutConstraint{
+    func changeMultiplier(value : CGFloat) -> NSLayoutConstraint{
+        
+        NSLayoutConstraint.deactivate([self]) // 비활성화
+        let newConstraint = NSLayoutConstraint.init(item: self.firstItem, attribute: self.firstAttribute, relatedBy: self.relation, toItem: self.secondItem, attribute: self.secondAttribute, multiplier: value, constant: self.constant)
+        
+        newConstraint.priority = self.priority
+        newConstraint.shouldBeArchived = self.shouldBeArchived
+        newConstraint.identifier = self.identifier
+        
+        NSLayoutConstraint.activate([newConstraint])
+        
+        return newConstraint
+    }// NSLayoutConstraint.multiplier 은 get only property이기 때문에 값을 적용하기 위해서는 새로운 constraint를 만들고 대입해야 한다
+}
