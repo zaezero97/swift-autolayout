3dfe8ed690060d7c7e492e39ea56895e62f251bc
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-04 00:03:43 +0900

    add animation code to ibaction func(selectBtn)
    2021 - 11 - 4

--- Switch_Custom/Switch_Custom/ViewController.swift
+++ Switch_Custom/Switch_Custom/ViewController.swift
@@ -12,12 +12,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var switchBG: UIView!
     @IBOutlet weak var switchBtn: UIButton!
     @IBOutlet weak var switchBtnCenterX: NSLayoutConstraint!
-    
+    var left = CGFloat.zero
+    var right = CGFloat.zero
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        left = -((switchBG.frame.width / 2) - (switchBtn.frame.width / 2))
+        right = (switchBG.frame.width / 2) - (switchBtn.frame.width / 2)
+        
+        switchBG.layer.cornerRadius = switchBG.frame.height / 2
+        switchBtn.layer.cornerRadius = switchBtn.frame.width / 2
+        switchBtnCenterX.constant = left
+    }
+    @IBAction func selectBtn(_ sender: Any) {
+        if switchBtnCenterX.constant == left{
+            switchBtnCenterX.constant = right
+        }else{
+            switchBtnCenterX.constant = left
+        }
+        
+        UIView.animate(withDuration: 1) {
+            self.switchBG.layoutSubviews()
+        }
+    }
+    
 }
 
