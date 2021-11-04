71dd365891d8c1ef94dba39be9d9592462d69c50
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-04 14:22:36 +0900

    add chat bubble cell xib
    2021 - 11 -4

---
+++ ChatingApp/ChatingApp/MyCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyCell.swift
+//  ChatingApp
+//
+//  Created by 재영신 on 2021/11/04.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ChatingApp/ChatingApp/YourCell.swift
@@ -0,0 +1,23 @@
+//
+//  YourCell.swift
+//  ChatingApp
+//
+//  Created by 재영신 on 2021/11/04.
+//
+
+import UIKit
+
+class YourCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
