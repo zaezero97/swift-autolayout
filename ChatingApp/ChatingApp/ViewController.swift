12fb061146c1fa2d00819d9f042170c883217c66
zaezero97 <83381672+zaezero97@users.noreply.github.com>
2021-11-05 09:56:15 +0900

    add keyboard notification
    2021 - 11 - 5

--- ChatingApp/ChatingApp/ViewController.swift
+++ ChatingApp/ChatingApp/ViewController.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputViewBottomMargin: NSLayoutConstraint!
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var inputTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        chatTableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "myCell")
+        chatTableView.register(UINib(nibName: "YourCell", bundle: nil), forCellReuseIdentifier: "yourCell")
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
         // Do any additional setup after loading the view.
+    }
+    @IBAction func sendString(_ sender: Any) {
+    }
+}
+extension ViewController{
+    @objc func keyboardWillShow(noti : Notification){
+        print("will show")
+        let notiInfo = noti.userInfo!
+        let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        
+        inputViewBottomMargin.constant = keyboardFrame.size.height
+    }
+    @objc func keyboardDidHide(noti : Notification){
+        print("did hide")
+        let notiInfo = noti.userInfo!
+        let keyboardFrame = notiInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        
+        inputViewBottomMargin.constant = 0
     }
 }
 
